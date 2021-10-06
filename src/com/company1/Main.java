d70010099fcd9e9f330a969af3fb899546efee57
Destice <r.chron@wp.pl>
2021-10-06 22:29:30 +0200

    1

---
+++ src/com/company1/Main.java
@@ -0,0 +1,10 @@
+package com.company1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        var name= "Rafal";
+        System.out.println("Hello " + name);
+    }
+}
